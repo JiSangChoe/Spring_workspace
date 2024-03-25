89d72e2ceca5c0fbe0056b959319e3d54fb74c37
JiSangChoe <chlwltkd123@gmail.com>
2024-03-26 02:17:00 +0900

    스프링부트 개인공부

---
+++ testproject/src/test/java/com/jisang/testproject/TestprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jisang.testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
