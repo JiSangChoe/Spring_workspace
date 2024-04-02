6ac36444306ad477144b8217599a77035660b3db
JiSangChoe <chlwltkd123@gmail.com>
2024-04-02 16:01:06 +0900

    2024.04.02.15:50 성공에 대한ResponseEntity  Dto code, message 반복되는 ResponseEntity ResponseDto에 넣음

--- board/src/main/java/com/example/board/dto/response/ResponseDto.java
+++ board/src/main/java/com/example/board/dto/response/ResponseDto.java
@@ -1,5 +1,8 @@
 package com.example.board.dto.response;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,4 +11,10 @@ import lombok.Getter;
 public class ResponseDto {
     private String code;
     private String message;
+
+    // 모든 API에서 이렇게 만듬 데이터베이스 실패에대한 코드 그래서 DTO에 넣었다!..
+    public static ResponseEntity<ResponseDto> databaseError() {
+        ResponseDto body = new ResponseDto(ResponseCode.DATABASE_ERROR, ResponseMessage.DATABASE_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+    }
 }
--- board/src/main/java/com/example/board/dto/response/board/GetLatestListResponseDto.java
+++ board/src/main/java/com/example/board/dto/response/board/GetLatestListResponseDto.java
@@ -1,6 +1,13 @@
 package com.example.board.dto.response.board;
 import java.util.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
+import com.example.board.dto.response.ResponseCode;
 import com.example.board.dto.response.ResponseDto;
+import com.example.board.dto.response.ResponseMessage;
 import com.example.board.dto.response.board.item.BoardListItem;
 
 import lombok.Getter;
@@ -9,8 +16,14 @@ import lombok.Getter;
 public class GetLatestListResponseDto extends ResponseDto{
     private List<BoardListItem> latestList;
 
-    public GetLatestListResponseDto(String code, String message, List<BoardListItem> latestList){
-        super(code, message);
+    private GetLatestListResponseDto(List<BoardListItem> latestList){
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
         this.latestList = latestList;
     }
+    // 왜 static을 쓸까 인스턴스를 만드는데
+    public static ResponseEntity<GetLatestListResponseDto> success(List<BoardListItem> latestList) {
+        GetLatestListResponseDto body = new GetLatestListResponseDto(latestList); 
+        return ResponseEntity.status(HttpStatus.OK).body(body);
+    }
+    
 }
